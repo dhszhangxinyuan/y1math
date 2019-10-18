257285753a6d2281ffb642a1abb3a47b1612990c
MinJiaTse <48120696+MinJiaTse@users.noreply.github.com>
2019-10-18 16:50:57 +0800

    Update t01primeshcflcm_evennumbers.py

--- t01primeshcflcm_evennumbers.py
+++ t01primeshcflcm_evennumbers.py
@@ -4,3 +4,15 @@ num = 123
 for i in range(1, num+1):
   if i % 2 == 0:
     print(i, end=' ')
+    
+    
+    
+    
+    
+    
+num = 450
+for i in range(1, num+1):
+  if i % 2 == 0:
+    print(i, end=' ')
+
+
